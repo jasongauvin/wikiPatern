96b579003a1061cb5cc359cfa394622e2aeaa494
Corto Dufour <cortodufour@gmail.com>
2020-07-22 11:21:48 +0200

    Server Base (Gin)

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/callmenapo/wikiPattern
 go 1.14
 
 require (
-	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/caarlos0/env/v6 v6.3.0
+	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/jinzhu/gorm v1.9.15 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"github.com/caarlos0/env/v6"
+	"github.com/gin-gonic/gin"
 	"log"
 )
-
 
 type config struct {
 	DbUser     string `env:"DB_USER"`
@@ -14,7 +14,24 @@ type config struct {
 	DbName     string `env:"DB_NAME"`
 }
 
+func HandleRequest(){
+	router := gin.New()
+
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	router.POST("/", func(c *gin.Context){
+		log.Printf("Wassup ?")
+	})
+
+	// Listen and serve on 0.0.0.0:8080
+	log.Fatal(router.Run(":8080"))
+}
+
 func main() {
+
+	HandleRequest()
+
 	cfg := config{}
 	if err := env.Parse(&cfg); err != nil {
 		log.Fatal(err)
