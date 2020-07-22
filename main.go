a5963fbc8990ceca694c7d8d84dcf93b67ad1555
Jacques Maarek <jacques.maarek@hetic.net>
2020-07-22 11:10:24 +0200

    Feat: Added config package and post model.

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"github.com/caarlos0/env/v6"
+)
+
+
+type config struct {
+	DbUser     string `env:"DB_USER"`
+	DbPassword string `env:"DB_PASSWORD"`
+	DbPort     int    `env:"DB_PORT" envDefault:"3306"`
+	DbHost     string `env:"DB_HOST"`
+	DbName     string `env:"DB_NAME"`
+}
+
+func main() {
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(err)
+	}
+	// Database initialization
+	config.InitializeDb(cfg.DbUser, cfg.DbPassword, cfg.DbHost, cfg.DbName, cfg.DbPort)
+}
